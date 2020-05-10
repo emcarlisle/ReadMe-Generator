f547b370bb8e48d103a6fb51c227e66f7e8c3f3a
Emily Carlisle <emilydeluna96@gmail.com>
2020-05-10 14:51:24 -0500

    generating markdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,25 @@
 function generateMarkdown(data) {
   return `
-# ${data.title}
-# ${data.description}
+  # **${data.title.trim()}**\n
+  ![Github](https://img.shields.io/github/last-commit/${data.username.trim()}/${data.title.trim()})\n
+  ## **Description**\n ${data.description.trim()}\n
+  ## **Table of Contents**\n
+  * Installation\n
+  * Usage\n
+  * License
+  * Contributing
+  * Tests
+  * Questions
+  ## **Installation**\n 
+  To install necessary dependencies, run the following command:\n
+  ${data.install.trim()}\n
+  ## **Usage**\n ${data.usage.trim()}
+  ## **License**\n
+  This project is licensed under the ${data.license.trim()} license.\n
+  ## **Contributing**\n
+  ${data.contribution.trim()}\n
+
+
 
 
 `;
